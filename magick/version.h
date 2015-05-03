3c0a9729f119ad05fb72274c84add529f11b2b70
Bastien ROUCARIÈS <roucaries.bastien@gmail.com>
2015-05-03 13:05:50 +0200

    Add upstream tar.xz for version 6.9.1.2

--- magick/version.h
+++ magick/version.h
@@ -27,7 +27,7 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2015 ImageMagick Studio LLC"
-#define MagickSVNRevision  "18398"
+#define MagickSVNRevision  "18428"
 #define MagickLibVersion  0x691
 #define MagickLibVersionText  "6.9.1"
 #define MagickLibVersionNumber  2,0,0
@@ -48,11 +48,11 @@ extern "C" {
 #define MagickppLibAddendum  "-2"
 #define MagickppLibInterface  6
 #define MagickppLibMinInterface  6
-#define MagickReleaseDate  "2015-04-14"
+#define MagickReleaseDate  "2015-04-19"
 #define MagickChangeDate   "20140409"
 #define MagickFeatures "DPC OpenMP"
-#define MagickDelegates "bzlib djvu mpeg fftw fpx fontconfig freetype jbig jng jpeg lcms lzma openexr pango png ps tiff webp x xml zlib"
-#define MagickHomeURL  "file:///usr/local/share/doc/ImageMagick-6/index.html"
+#define MagickDelegates "bzlib djvu mpeg fftw fpx fontconfig freetype jbig lzma openexr pango png ps tiff webp x xml zlib"
+#define MagickHomeURL  "file:///usr/share/doc/ImageMagick-6/index.html"
 #define MagickAuthoritativeLicense  \
   "http://www.imagemagick.org/script/license.php"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
